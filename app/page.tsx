34709757b2e249a90a06b2d2e976d8de25698b31
sharanvijayan13 <sharanvijayan05@gmail.com>
2025-06-30 22:48:02 +0530

    Deploy

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4 text-center">
+      <h1 className="text-6xl font-bold mb-4 text-center">
         Shaflix
       </h1>
       
-      <p className="text-lg mb-6 text-gray-700">
-        Let's explore Next.js with shadcn and Tailwind!
+      <p className="text-lg mb-6 text-gray-800">
+        I'm gonna build Shaflix with NextJS
       </p>
 
       <Button variant="default">
-        Get Started
+        Lets Begin
       </Button>
     </div>
   );
